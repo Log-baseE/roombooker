15b007c1e768bf543d1ecbeed8e1f2f7836cf1f2
NickyLogan <laurentiusdominick@yahoo.com>
2018-10-23 15:46:18 +0700

    Add adminator template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,6 +9,8 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+
         <!-- Styles -->
         <style>
             html, body {
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix
+   .js('resources/js/app.js', 'public/js')
+   .js('resources/adminator/scripts/index.js', 'public/js/admin-vendor.js')
+   .sass('resources/sass/app.scss', 'public/css')
+   .sass('resources/adminator/styles/index.scss', 'public/css/admin-vendor.css')
+   .copyDirectory('resources/adminator/static', 'public/assets/static');
