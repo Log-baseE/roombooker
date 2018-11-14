3ec1613a7566945ee7dae4fbeb76b591217a2287
NickyLogan <laurentiusdominick@yahoo.com>
2018-11-14 08:00:10 +0700

    Modify template structure

---
+++ resources/views/layouts/front/nav.blade.php
@@ -0,0 +1,33 @@
+<nav class="main-nav">
+    <div class="container">
+        <a href="#" class="logo">
+            LOGO
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarLinks" aria-controls="navbarLinks" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div id="navbarLinks" class="navbar-links left-links">
+            <ul>
+                <li><a href="rooms">Rooms</a></li>
+                <li class="dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <a href="#" class="dropdown-toggle" id="navbarDropdown" role="button">Support</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="faq">FAQ</a>
+                        <a class="dropdown-item" href="contact">Contact us</a>
+                    </div>
+                </li>
+                <li role="separator" class="divider"></li>
+            </ul>
+        </div>
+        <div id="navbarLinks" class="navbar-links right-links">
+            <ul>
+                <li>
+                    <a class="login">Log in</a>
+                </li>
+                <li>
+                    <a class="signup">Sign up</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/layouts/front/template.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="The room booking solution for event organizers">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title') · RoomBooker</title>
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+</head>
+<body>
+    @include('layouts.front.nav')
+    <div class="main-body">
+        @yield('content')
+    </div>
+    @include('layouts.front.footer')
+    <script src="{{ mix('js/app.js' )}}"></script>
+</body>
+</html>
