5c604e87febcafaabb343b29fc9589cfbe9824ef
NickyLogan <laurentiusdominick@yahoo.com>
2018-11-27 09:26:48 +0700

    Add auth

--- app/Http/Controllers/MakeSignature.php
+++ app/Http/Controllers/MakeSignature.php
@@ -7,6 +7,16 @@ use Illuminate\Http\Request;
 class MakeSignature extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
      * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
